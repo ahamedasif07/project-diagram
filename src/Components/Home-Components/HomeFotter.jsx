d609e13600aa941a11a85c5f63f83355fd51b586
rxasif07 <rxasif31@gmail.com>
2024-08-26 11:06:33 +0600

    fainal tuch home page

--- src/Components/Home-Components/HomeFotter.jsx
+++ src/Components/Home-Components/HomeFotter.jsx
@@ -7,15 +7,14 @@ const HomeFotter = () => {
     <div className="max-w-[1100px] pt-5 mx-auto">
       <div className="pt-8">
         <h2 className="text-center">About Us</h2>
-        <p className="text-center mt-3 text-[14px] text-gray-700 pb-5">
+        <p className="text-center mt-3 max-w-[700px] text-[14px] text-gray-700 pb-5 mx-auto">
           Diagram is a fashion brand that strives to create clothing that is not
           only stylish but also fashion-forward, versatile and affordable. Our
           mission is to empower individuals to express themselves through their
           clothing while feeling comfortable and confident.
         </p>
-        <hr />
       </div>
-      <div>
+      <div className="py-8">
         <h2 className="text-[18px] md:text-left text-center text-gray-700 pb-3 px-4">
           Join The Diagram ARMY
         </h2>
@@ -44,6 +43,13 @@ const HomeFotter = () => {
               alt=""
             />
           </div>
+        </div>
+        <hr />
+        <div className="flex gap-4 px-4 items-center pt-6 py-">
+          <h5 className="text-[8px] text-gray-700">2024, Diagram</h5>
+          <h5 className="text-[8px] text-gray-700">Refund policy</h5>
+          <h5 className="text-[8px] text-gray-700">Terms of service</h5>
+          <h5 className="text-[8px] text-gray-700">Contact information</h5>
         </div>
       </div>
     </div>
