b110f66cec16852b97e62daf797dd28be2c4139c
rxasif07 <rxasif31@gmail.com>
2024-08-26 10:57:34 +0600

    home page done

--- src/Components/Home-Components/HomeFotter.jsx
+++ src/Components/Home-Components/HomeFotter.jsx
@@ -1,6 +1,10 @@
+import fachbook from "../../../public/images/diagram-home-page/facebook 1.png";
+import instra from "../../../public/images/diagram-home-page/instagram 1.png";
+import linkdin from "../../../public/images/diagram-home-page/linkedin 1.png";
+
 const HomeFotter = () => {
   return (
-    <div className="max-w-[1100px] mx-auto">
+    <div className="max-w-[1100px] pt-5 mx-auto">
       <div className="pt-8">
         <h2 className="text-center">About Us</h2>
         <p className="text-center mt-3 text-[14px] text-gray-700 pb-5">
@@ -10,6 +14,37 @@ const HomeFotter = () => {
           clothing while feeling comfortable and confident.
         </p>
         <hr />
+      </div>
+      <div>
+        <h2 className="text-[18px] md:text-left text-center text-gray-700 pb-3 px-4">
+          Join The Diagram ARMY
+        </h2>
+        <div className="flex flex-col md:flex-row justify-between items-center pb-4 px-4">
+          <div>
+            <input
+              className="px-3 py-2 border-2 border-black"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="flex pt-4 md:py-0 gap-3">
+            <img
+              className="bg-black w-[25px] h-[25px] rounded-full border-2 border-black"
+              src={fachbook}
+              alt=""
+            />
+            <img
+              className="bg-black w-[25px] h-[25px] rounded-full border-2 border-black"
+              src={instra}
+              alt=""
+            />
+            <img
+              className="bg-black w-[25px] h-[25px] rounded-full border-2 border-black"
+              src={linkdin}
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
