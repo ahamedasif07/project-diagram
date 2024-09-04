c82b6d7bc2a5c4525bf97b70776436dfbd4d5221
rxasif07 <rxasif31@gmail.com>
2024-09-04 10:41:51 +0600

    nav scarch fainal tuch

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -24,52 +24,53 @@ const NavBar = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  console.log(isScarch);
 
   return (
     <div>
+      {/* Top Bar */}
       <div className="text-center bg-white py-2">
         <h2 className="text-[13px] font-[13px]">One Lifestyle For Everyone</h2>
       </div>
-      {/* ________middel nav__________ */}
+
+      {/* Navbar */}
       <div className="relative">
         <div className={isFixed ? "fixed top-0 w-full z-50" : ""}>
           <div className="">
             <div className="middel-nav">
               <div className="bg-[#242833] flex justify-around items-center py-4">
-                <h2 className="text-white text-xl">
-                  <RiMenu2Fill />
-                </h2>
-                <img src={navLogo} alt="Logo" />
-                <div className="flex items-center gap-3">
-                  <h2
-                    onClick={() => setIsScarch(true)}
-                    className="text-white text-xl"
-                  >
-                    <ImSearch />
-                  </h2>
-                  <h2 className="text-white text-xl">
-                    <BsHandbag />
-                  </h2>
-                </div>
+                {!isScarch && (
+                  <>
+                    <h2 className="text-white text-xl">
+                      <RiMenu2Fill />
+                    </h2>
+                    <img src={navLogo} alt="Logo" />
+                    <div className="flex items-center gap-3">
+                      <h2
+                        onClick={() => setIsScarch(true)}
+                        className="text-white text-xl"
+                      >
+                        <ImSearch />
+                      </h2>
+                      <h2 className="text-white text-xl">
+                        <BsHandbag />
+                      </h2>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           </div>
         </div>
+
+        {/* Search Bar */}
         <div
-          className={
+          className={`${
             isScarch
-              ? "container left-0 right-0 mx-auto transition-all duration-300 absolute bottom-3 ease-in-out bg-[#242833] py-2"
-              : ""
-          }
+              ? " fixed transition-all duration-500 ease-in-out top-0 left-0 right-0 w-full z-50 bg-[#242833] py-2 "
+              : "hidden"
+          }`}
         >
-          <div
-            className={
-              isScarch
-                ? "max-w-[600px] bg-[#242833] container mx-auto  px-2 flex items-center gap-4 z-100"
-                : "hidden"
-            }
-          >
+          <div className="max-w-[600px] mx-auto py-4 px-2 flex items-center gap-4">
             <input
               className="py-2 px-4 w-full md:w-[600px] border-b-2 border-transparent focus:border-b-2 focus:border-black outline-none"
               type="search"
@@ -79,7 +80,7 @@ const NavBar = () => {
               onClick={() => setIsScarch(false)}
               className="text-white text-xl cursor-pointer"
             >
-              <RxCross2></RxCross2>
+              <RxCross2 />
             </h2>
           </div>
         </div>
