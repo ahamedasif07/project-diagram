8abe8d18f044805113615659e3dd6b9beee672e5
rxasif07 <rxasif31@gmail.com>
2024-09-07 17:48:32 +0600

    done paddinf margin

--- src/Components/ProductInfo.jsx
+++ src/Components/ProductInfo.jsx
@@ -2,11 +2,9 @@
 
 const ProductInfo = ({ title, paragraph, color }) => {
   return (
-    <div className="px-4 max-w-[1100px] mx-auto  pb-3">
+    <div className=" max-w-[1100px] mx-auto  pb-3">
       <div className=" max-w-[500px]">
-        <h2 className={`text-4xl xm:px-4 px-6 md:px-0 pb-2 ${color}`}>
-          {title}
-        </h2>
+        <h2 className={`text-4xl px-4 md:px-0  pb-2 ${color}`}>{title}</h2>
 
         {paragraph.slice(0, 150) && (
           <p className="text-[15px] pt-4 pb-2 text-gray-700 px-4 md:px-0">
