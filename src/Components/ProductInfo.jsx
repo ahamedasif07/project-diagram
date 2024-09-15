d1bedc49ecca3a5373f1d4b410ed0e9d0a844352
rxasif07 <rxasif31@gmail.com>
2024-09-15 18:42:27 +0700

    text padding part sholve

--- src/Components/ProductInfo.jsx
+++ src/Components/ProductInfo.jsx
@@ -2,9 +2,9 @@
 
 const ProductInfo = ({ title, paragraph, color }) => {
   return (
-    <div className=" max-w-[1100px] mx-auto  pb-3">
-      <div className=" max-w-[500px] px-4  md:px-4 ">
-        <h2 className={`text-4xl px-4  pb-2 ${color}`}>{title}</h2>
+    <div className=" md:max-w-[1100px] mx-auto px-4   pb-3">
+      <div className=" max-w-[500px]    ">
+        <h2 className={`text-4xl px-4 md:px-0 pb-2 ${color}`}>{title}</h2>
 
         {paragraph.slice(0, 150) && (
           <p className="text-[15px] pt-4 pb-2 text-gray-700 px-4 md:px-0">
