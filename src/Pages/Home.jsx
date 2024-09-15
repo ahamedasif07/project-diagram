0b9add61a019c4a8a0fdc54315297d45f470fd49
rxasif07 <rxasif31@gmail.com>
2024-09-15 20:18:00 +0700

    loding off

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,7 +12,7 @@ import { ProductContext } from "../LayOut";
 const Home = () => {
   const AllProducts = useContext(ProductContext);
   console.log("from contex", AllProducts);
-  const [products, setProducts] = useState([]);
+
   const [newAraivles, setNewAraivles] = useState([]);
   const [showAll, setShowAll] = useState(8);
   const [demoProduct, setDemoproduct] = useState([]);
@@ -41,61 +41,55 @@ const Home = () => {
 
   return (
     <div>
-      {isLoading ? (
-        <div className="w-[100%] h-[100vh] flex justify-center items-center">
-          <Spiner></Spiner>
-        </div>
-      ) : (
+      <div>
+        <Header></Header>
         <div>
-          <Header></Header>
-          <div>
-            <div className="px-4 max-w-[1100px] mx-auto pt-16 pb-3">
-              <ProductInfo
-                color={`text-gray-700`}
-                title={"Summer Arrivals"}
-                paragraph={
-                  "Embrace the season with our latest collection and make this summer your most stylish one yet!"
-                }
-              ></ProductInfo>
-            </div>
-            <div className="grid px-4 max-w-[1100px]  justify-center pt-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-1 mx-auto md:gap-5">
-              {newAraivles?.slice(0, showAll).map((product, idx) => (
-                <Product key={idx} product={product}></Product>
+          <div className="px-4 max-w-[1100px] mx-auto pt-16 pb-3">
+            <ProductInfo
+              color={`text-gray-700`}
+              title={"Summer Arrivals"}
+              paragraph={
+                "Embrace the season with our latest collection and make this summer your most stylish one yet!"
+              }
+            ></ProductInfo>
+          </div>
+          <div className="grid px-4 max-w-[1100px]  justify-center pt-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-1 mx-auto md:gap-5">
+            {newAraivles?.slice(0, showAll).map((product, idx) => (
+              <Product key={idx} product={product}></Product>
+            ))}
+          </div>
+        </div>
+        <div className={showAll >= newAraivles.length ? "hidden" : "block"}>
+          <div className="flex justify-center py-[44px]">
+            <button
+              onClick={handleViewAll}
+              className="bg-black text-white px-3 py-2 text-center"
+            >
+              View All
+            </button>
+          </div>
+        </div>
+        <div className="max-w-[1100px] mx-auto">
+          <div className="px-4 max-w-[1100px] mx-auto pt-10">
+            <h2 className="text-xl px-4 text-[22px] md:px-0 pb-2 font-semibold ">
+              Product Collection
+            </h2>
+          </div>
+          <div className="">
+            <div className="grid px-4  justify-center pt-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-1  md:gap-5">
+              {demoProduct.map((demo) => (
+                <div className="px-4 md:px-0 pb-5" key={demo.id}>
+                  <img className="w-full" src={demo.image} alt="" />
+                </div>
               ))}
             </div>
           </div>
-          <div className={showAll >= newAraivles.length ? "hidden" : "block"}>
-            <div className="flex justify-center py-[44px]">
-              <button
-                onClick={handleViewAll}
-                className="bg-black text-white px-3 py-2 text-center"
-              >
-                View All
-              </button>
-            </div>
-          </div>
-          <div className="max-w-[1100px] mx-auto">
-            <div className="px-4 max-w-[1100px] mx-auto pt-10">
-              <h2 className="text-xl px-4 text-[22px] md:px-0 pb-2 font-semibold ">
-                Product Collection
-              </h2>
-            </div>
-            <div className="">
-              <div className="grid px-4  justify-center pt-3 lg:grid-cols-4 md:grid-cols-3 grid-cols-1  md:gap-5">
-                {demoProduct.map((demo) => (
-                  <div className="px-4 md:px-0 pb-5" key={demo.id}>
-                    <img className="w-full" src={demo.image} alt="" />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          {/* video components */}
-          <Video></Video>
-          <Details></Details>
-          <HomeFotter></HomeFotter>
         </div>
-      )}
+        {/* video components */}
+        <Video></Video>
+        <Details></Details>
+        <HomeFotter></HomeFotter>
+      </div>
     </div>
   );
 };
